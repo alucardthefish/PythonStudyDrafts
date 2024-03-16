7ccd12c3e7b9f4dbc326e4f762effe6ddf55e256
alucardthefish <sergiop18@gmail.com>
2024-03-16 17:37:23 -0500

    Add 5 more fstring tricks

--- fstring_tricks.py
+++ fstring_tricks.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, date
 
 
 # Trick 1
@@ -39,3 +39,47 @@ my_var: str = "Bob says hi"
 
 print(f"{my_var = }")
 print(f"{a + b = }")
+
+
+# Trick 6 Scientific notation
+
+big_number: int = 1_620_000_000
+
+print(f"{big_number:e}")  # 1.620000e+09
+print(f"{big_number:.2e}")  # 1.62e+09
+
+
+# Trick 7
+
+date_spec: str = "%d.%m.%Y"
+print(f"{now:{date_spec}}")
+
+num: float = 1000000.1234567
+spec: str = ",.2f"
+print(f"{num:{spec}}")
+
+
+# Trick 8
+
+custom_folder: str = "my_folder"
+path: str = fr"\Users\fico\documents\{custom_folder}"
+print(path)
+
+
+# Trick 9
+
+a: float = 0.1
+b: float = 0.2
+
+print(f"{a + b = :.1f}")
+
+
+# Trick 10
+
+banana: str = "🍌"
+name: str = "Bob"
+today: date = datetime.now().date()
+
+print(f"[{today!s}] {name!s} says: {banana!s}")
+print(f"[{today!r}] {name!r} says: {banana!r}")
+print(f"[{today!a}] {name!a} says: {banana!a}")
